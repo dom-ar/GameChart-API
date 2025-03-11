0cc343d6ccdb35391d3ab44faff5c1773c7119d5
dom-ar <domprotected@gmail.com>
2025-03-11 19:28:00 +0200

    Add reusable sorting/ordering option

---
+++ Repository/Extensions/Utility/OrderQueryBuilder.cs
@@ -0,0 +1,36 @@
+﻿using Entities.Models;
+using System.Reflection;
+using System.Text;
+
+namespace Repository.Extensions.Utility;
+
+public static class OrderQueryBuilder
+{
+    public static string CreateOrderQuery<T>(string orderByQueryString)
+    {
+        var orderParams = orderByQueryString.Trim().Split(',');
+        var propertyInfos = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        var orderQueryBuilder = new StringBuilder();
+
+        foreach (var param in orderParams)
+        {
+            if (string.IsNullOrWhiteSpace(param))
+                continue;
+
+            var trimmedParam = param.Trim();
+            var propertyFromQueryName = trimmedParam.Split(" ")[0];
+            var objectProperty = propertyInfos.FirstOrDefault(pi =>
+                pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+
+            if (objectProperty == null)
+                continue;
+
+            var direction = trimmedParam.EndsWith(" desc") ? "descending" : "ascending";
+            orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction}, ");
+        }
+
+        var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+
+        return orderQuery;
+    }
+}
